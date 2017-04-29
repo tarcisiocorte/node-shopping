4563da653ba6e9a5a2548b12724394a6bca3a5a9
Tarcisio Costa Corte de Paula <Tarcisio Costa Corte de Paula>
2017-04-29 19:07:42 -0300

    added the boady-parser, cookie-parser, morgan and stylus

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
-var express = require('express');
+var express = require('express'),
+    stylus = require('stylus'),
+    logger = require('morgan'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
+    passport = require('passport');
+
 
 var env = process.env.NODE_ENV = process.NODE_ENV || 'development';
 
 var app = express();
 
+function compile(str,path){
+    return stylus(str).set('filename', path);
+}
+
 app.set('views', __dirname + '/server/views');
 
 app.set('view engine','jade');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.set(stylus.middleware(
+    {
+        src: __dirname + '/public',
+        compile: compile
+    }
+));
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req, res){
     res.render('index');
