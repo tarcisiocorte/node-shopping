3ba18dde777726e2bf24f39d13e49a78177467ee
tarcisiocorte <tarcisiocorte@gmail.com>
2017-05-17 14:45:33 -0300

    layout login

--- node-frontend/src/app/nav/nav.component.ts
+++ node-frontend/src/app/nav/nav.component.ts
@@ -5,11 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class NavComponent {
 
 }
