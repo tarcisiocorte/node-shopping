7ef11696b2d370ba13865a5eebdd536e4d177a36
tarcisiocorte <tarcisiocorte@gmail.com>
2017-05-31 15:51:58 -0300

    CRUD complete using tests approach

--- node-backend/test/integration/app.js
+++ node-backend/test/integration/app.js
@@ -16,6 +16,7 @@ describe('Routes Books', () => {
 
     describe('Route GET /books', () => {
         it('should return a list of books', done =>{
+
             request
             .get('/books')
             .end((err, res) =>{
@@ -27,4 +28,73 @@ describe('Routes Books', () => {
             });
         });
     });
+
+    describe('Route GET /books/{id}', () => {
+        it('should return a book', done =>{
+            request
+            .get('/books/1')
+            .end((err, res) =>{
+
+                expect(res.body.id).to.be.eql(defaultBooks.id);
+                expect(res.body.name).to.be.eql(defaultBooks.name);
+                
+                done(err);
+            });
+        });
+    });
+
+    describe('Route POST /books', () => {
+        it('should create a book', done =>{       
+            const newBook = {
+                    id: 2,
+                    name: 'New Book'
+                }; 
+
+            request
+            .post('/books')
+            .send(newBook)
+            .end((err, res) =>{
+
+                expect(res.body.id).to.be.eql(newBook.id);
+                expect(res.body.name).to.be.eql(newBook.name);
+                
+                done(err);
+            });
+        });
+    });
+
+    describe('Route PUT /books/{id}', () => {
+        it('should update a book', done =>{       
+            const updatedBook = {
+                    id: 1,
+                    name: 'Updated Book'
+                }; 
+
+            request
+            .put('/books/1')
+            .send(updatedBook)
+            .end((err, res) =>{
+                
+                console.log('RESPONSE', res.body);
+                
+                expect(res.body).to.be.eql([1]);
+                
+                done(err);
+            });
+        });
+    });
+
+    describe('Route delete /books/{id}', () => {
+        it('delete a book', done =>{       
+            request
+            .delete('/books/1')
+            .end((err, res) =>{
+
+                expect(res.statusCode).to.be.eql(204);
+                
+                done(err);
+            });
+        });
+    });            
+
 });
